398ce9b87c7bec9c71bc7a1b1ddcfeb154b40194
Noob-H4cker <alsamiul123@gmail.com>
2023-10-29 14:25:27 +0600

    say digit problem solved

---
+++ Recursion/05.Say_Digit.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sayDigit(int n, string arr[])
+{
+    if (n == 0)
+    {
+        return;
+    }
+    int digit = n % 10;
+    n /= 10;
+
+    sayDigit(n, arr);
+    cout << arr[digit] << " ";
+}
+
+int main()
+{
+    int n = 421;
+    string arr[10] = {
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    };
+
+    sayDigit(n, arr);
+
+    return 0;
+}
