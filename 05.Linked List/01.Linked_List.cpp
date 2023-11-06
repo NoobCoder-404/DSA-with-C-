e14500882904a98b051df3d8084b4fbcbbeb3e8f
Noob-H4cker <alsamiul123@gmail.com>
2023-11-06 10:43:00 +0600

    inset head tail and position in linkedList

---
+++ 05.Linked List/01.Linked_List.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAtHead(Node *&head, int d)
+{
+    Node *temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void insertAtTail(Node *&tail, int d)
+{
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = tail->next;
+}
+
+void insertAtPosition(Node *&tail, Node *&head, int position, int d)
+{
+    if (position == 1)
+    {
+        insertAtHead(head, d);
+        return;
+    }
+    Node *temp = head;
+    int count = 1;
+
+    while (count < position - 1)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, d);
+        return;
+    }
+
+    Node *nodeToInsert = new Node(d);
+
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
+}
+
+void printNode(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *node1 = new Node(10);
+
+    Node *head = node1;
+    Node *tail = node1;
+    // printNode(head);
+
+    // insertAtHead(head, 12);
+    // printNode(head);
+
+    // insertAtHead(head, 15);
+    // printNode(head);
+
+    insertAtTail(tail, 12);
+
+    insertAtTail(tail, 15);
+    printNode(head);
+
+    cout << endl;
+
+    insertAtPosition(tail, head, 3, 14);
+    printNode(head);
+
+    cout << endl;
+
+    cout << "Head " << head->data << endl;
+    cout << "Tail " << tail->data << endl;
+
+    return 0;
+}
